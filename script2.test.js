69b2e3722ef032cc783b83c5863b04c508e4cb2f
IamZombiez <dereksiu216@gmail.com>
2018-07-25 14:04:51 -0700

    jest mocks

--- script2.test.js
+++ script2.test.js
@@ -2,7 +2,28 @@ const fetch = require('node-fetch');
 const swapi = require('./script2');
 
 it('calls swapi to get people'), () => {
-  swapi.getPeople(fetch).then(data => {
+  expect.assertions(1)
+  return swapi.getPeople(fetch).then(data => {
     expect(data.count).toEqual(87);
+  });
+})
+
+it('calls swapi', () => {
+  expect.assertions(2)
+  return swapi.getPeoplePromise(fetch).then(data => {
+    expect(data.count).toEqual(87)
+    expect(data.results.length).toBeGreaterThan(5)
   })
-}
+})
+
+// Mock
+
+it('getPeople and return counts and res', () => {
+  const mockFetch = jest.fn()
+    .mockReturnValue(Promise.resolve({
+      json: () => Promise.resolve({
+        count: 87,
+        results:[0,1,2,3,4,5]
+      })
+    }))
+})
